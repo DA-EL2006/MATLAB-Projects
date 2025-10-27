eacb1e9a2611b1c0ab72a242354756f2efae65d0
DA-EL2006 <davidkayode153@gmail.com>
2025-10-26 21:01:00 -0700

    Add files via upload

---
+++ Servo Motor, Joint Position Control, PID/servo_motor.m
@@ -0,0 +1,48 @@
+%Servo Motor Control.l
+%Parameters Definition 
+J = 0.01;  %Inertia
+b = 0.001;  %Viscous damping
+Kt = 0.05;   % Torque constant
+Ke = 0.05;  %Back EMF constant
+R = 2;    %Armature resistance
+
+%Getting the Transfer Function
+num = [Kt/R];
+den= [J (b+((Kt*Ke)/R))];
+
+G = tf(num, den);
+
+pole(G)
+bandwidth(G) 
+
+%PID Controller.
+H = pidtune(G, 'PID');
+Kp = H.Kp;
+Ki = H.Ki;
+Kd = H.Kd;
+
+%Checking the step, bode and margin plots
+figure;
+step(G);
+grid on;
+title('Open Loop Step Response');
+S = stepinfo(G);
+dcg = dcgain(G);
+
+%Margin plot a.k.a Bode Plot
+figure;
+title('Margin- gain and phase margin diagram ')
+margin(G);
+grid on;
+
+%Closed Loop Graphing
+Tcl = feedback(H*G, 1);   % closed-loop transfer (1 for unity feedback)
+figure;
+step(Tcl);
+grid on;   
+info = stepinfo(Tcl);   % rise time, settling time, overshoot
+p = pole(Tcl);                 % closed-loop poles
+bw = bandwidth(Tcl);           % closed-loop bandwidth
+margin(H*G)                   % open-loop margins
+
+open_system()
